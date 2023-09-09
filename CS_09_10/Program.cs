17579c60de97805d036263aaa53ccea7050deecd
aliya <aliya.itstep@gmail.com>
2023-09-09 16:33:12 +0600

    Добавьте файлы проекта.

---
+++ CS_09_10/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CS_09_10
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ранее в одном из практических заданий вы создавали класс «Shop».
+            // Добавьте к уже созданному классу информацию о площади магазина.
+            // Выполните перегрузку + (для увеличения площади магазина на указанную величину),
+            // — (для уменьшения площади магазина на указанную величину),
+            // == (проверка на равенство площадей магазинов),
+            // < и > (проверка на меньше или больше площади магазина),
+            // != и Equals. Используйте механизм свойств для полей класса.
+
+            Shop shop1 = new Shop(100);
+            Shop shop2 = new Shop(150);
+
+            Console.WriteLine("Shop1: " + shop1);
+            Console.WriteLine("Shop2: " + shop2);
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"Operator + : {shop1} + 30 = {shop1 + 30}");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"Operator - : {shop2} - 70 = {shop2 - 70}");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"Operator < : {shop1} < {shop2} : {shop1 < shop2}");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"Operator > : {shop1} > {shop2} : {shop1 > shop2}");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Operator == : " + (shop1 == shop2));
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Operator != : " + (shop1 != shop2));
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Equals Method: " + shop1.Equals(shop2));
+        }
+    }
+}
---
+++ CS_09_10/Shop.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_09_10
+{
+    public class Shop
+    {
+        private float s; // площадь
+
+        public float S { get { return s; } set { s = value; } }
+
+        public Shop(float s)
+        {
+            S = s;
+        }
+
+        // Перегрузка операторов
+        // + (для увеличения площади магазина на указанную величину)
+        public static Shop operator +(Shop shop, float num)
+        {
+            Shop result = new Shop(0f);
+            result.S = shop.S + num;
+            return result;
+        }
+
+        // - (для уменьшения площади магазина на указанную величину)
+        public static Shop operator -(Shop shop, float num)
+        {
+            Shop result = new Shop(0f);
+            result.S = shop.S - num;
+            return result;
+        }
+
+        // == (проверка на равенство площадей магазинов)
+        public static bool operator ==(Shop shop1, Shop shop2)
+        {
+            return shop1.S == shop2.S;
+        }
+
+        // != (проверки на неравенство площадей магазинов)
+        public static bool operator !=(Shop shop1, Shop shop2)
+        {
+            return shop1.S != shop2.S;
+        }
+
+        // < (проверка на меньше площади магазина)
+        public static bool operator <(Shop shop1, Shop shop2)
+        {
+            return shop1.S < shop2.S;
+        }
+
+        // > (проверка на больше площади магазина)
+        public static bool operator >(Shop shop1, Shop shop2)
+        {
+            return shop1.S > shop2.S;
+        }
+
+
+        // Перегрузка метода Equals для сравнения объектов класса Shop
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            Shop temp = (Shop)obj;
+            //return this.S == temp.S;
+            return this.S.Equals(temp.S);
+        }
+
+        // Переопределение GetHashCode для поддержки перегруженного Equals
+        public override int GetHashCode()
+        {
+            return S.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{S}";
+        }
+    }
+}
